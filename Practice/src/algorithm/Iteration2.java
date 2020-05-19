e59f024f62ec61111ca0c6f03bf183eabb2d06f3
user <user@KimYoonSuk>
2020-05-19 23:09:49 +0900

    2020.05.19

--- Practice/src/algorithm/Iteration2.java
+++ Practice/src/algorithm/Iteration2.java
@@ -1,28 +1,31 @@
 package algorithm;
 
-public class Iteration {
+public class Iteration2 {
 
 	public static void main(String[] args) {
-		// 별 찍기 최종 
+		int num = 0;
 		for (int i = 0; i < 5; i += 1) {
 			if (i == 0) {
 				for (int j = 0; j < 4; j = j + 1) {
 					System.out.print(" ");
 				}
-				System.out.print("*");
+				System.out.print(num);
 			} else if (i == 4) {
 				for (int j = 0; j < 9; j = j + 1) {
-					System.out.print("*");
+					num = num + 1;
+					System.out.print(num % 10);
 				}
 			} else {
 				for(int j = 0; j < 4 - i; j = j + 1 ) {
 					System.out.print(" ");
 				}
-				System.out.print("*");
+				num = num + 1;
+				System.out.print(num);
 				for(int j = 0; j < 2*i -1; j += 1) {
 					System.out.print(" ");
 				}
-				System.out.print("*");
+				num = num + 1;
+				System.out.print(num);
 			}
 			System.out.print("\n");
 			
