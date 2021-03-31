a834132eebd7f9b99e79829950ce8c125b11ea3b
yoonsuk <yskim62100@gamil.com>
2021-03-31 11:37:03 +0900

    2021.03.31 updating algorithm practice

---
+++ Practice/src/algorithm/NumberRectangle.java
@@ -0,0 +1,35 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class NumberRectangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자사각형의 높이로 사용할 숫자(100아하)를 입력하시오: ");
+		int n = sc.nextInt();
+		System.out.print("숫자사각형의 너비로 사용할 숫자(100이하)를 입력하시오: ");
+		int m = sc.nextInt();
+		
+		if(n > 100) {
+			System.out.println("100이하의 정수를 입력하세요!");
+			System.out.print("숫자사각형의 높이로 사용할 숫자(100아하)를 입력하시오: ");
+			n = sc.nextInt();
+		}
+		
+		if(m > 100) {
+			System.out.println("100이하의 정수를 입력하세요!");
+			System.out.print("숫자사각형의 너비로 사용할 숫자(100아하)를 입력하시오: ");
+			m = sc.nextInt();
+		}
+		
+		int sum = 1;
+		for(int i=0; i < n; i++){
+		    for(int j = 0; j < m; j++){
+		    	System.out.printf("%d ", sum++);
+		    }
+		    System.out.println();
+		}		
+	}
+
+}
