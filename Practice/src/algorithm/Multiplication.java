b3caa7cc543fa0096b72f16aa9e8f3499b329b6b
yoonsuk <yskim62100@gamil.com>
2021-04-07 13:19:24 +0900

    2021.04.07

---
+++ Practice/src/algorithm/Multiplication.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("곱셈에 사용할 첫번째 3자리 자연수를 입력하시오: ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		System.out.print("곱셈에 사용할 두번째 3자리 자연수를 입력하시오: ");
+		String m = sc.nextLine();
+		
+		int result;
+		
+		String var3 = m.substring(2);
+		int num3 = Integer.parseInt(var3) * n;
+		
+		String var4 = m.substring(1, 2);
+		int num4 = Integer.parseInt(var4) * n;
+		
+		String var5 = m.substring(0, 1);
+		int num5 = Integer.parseInt(var5) * n;
+		
+		int num6 = num3 + (num4 * 10) + (num5 * 100);
+		
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(num6);
+	}
+
+}
---
+++ Practice/src/algorithm/NumberInNumber.java
@@ -0,0 +1,87 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class NumberInNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("곱셈에 사용할 첫번째 3자리 자연수(100이상, 1000미만)를 입력하시오: ");
+		int n1 = sc.nextInt();
+		
+		System.out.print("곱셈에 사용할 두번째 3자리 자연수(100이상, 1000미만)를 입력하시오: ");
+		int n2 = sc.nextInt();
+		
+		System.out.print("곱셈에 사용할 세번째 3자리 자연수(100이상, 1000미만)를 입력하시오: ");
+		int n3 = sc.nextInt();
+	
+	
+		while(n1 < 100 | n1 >= 1000) {
+			System.out.println("3자리 자연수를 입력하세요!");
+			System.out.print("곱셈에 사용할 3자리 자연수(100이상, 1000미만)를 입력하시오: ");
+			n1 = sc.nextInt();	
+		}
+		
+		while(n2 < 100 | n2 >= 1000) {
+			System.out.println("3자리 자연수를 입력하세요!");
+			System.out.print("곱셈에 사용할 3자리 자연수(100이상, 1000미만)를 입력하시오: ");
+			n2 = sc.nextInt();	
+		}
+		
+		while(n3 < 100 | n3 >= 1000) {
+			System.out.println("3자리 자연수를 입력하세요!");
+			System.out.print("곱셈에 사용할 3자리 자연수(100이상, 1000미만)를 입력하시오: ");
+			n3 = sc.nextInt();	
+		}
+		
+		String temp = n1 * n2 * n3 + "";
+		String[] result = null;
+		for(int i=0; i<temp.split("").length; i++) {
+			result = temp.split("");
+		}
+		
+		int[] cnt = new int[10];
+		for(String num : result) {
+			switch(num) {
+			case "0":
+				cnt[0]++;
+				break;
+			case "1":
+				cnt[1]++;
+				break;
+			case "2":
+				cnt[2]++;
+				break;
+			case "3":
+				cnt[3]++;
+				break;
+			case "4":
+				cnt[4]++;
+				break;
+			case "5":
+				cnt[5]++;
+				break;
+			case "6":
+				cnt[6]++;
+				break;
+			case "7":
+				cnt[7]++;
+				break;
+			case "8":
+				cnt[8]++;
+				break;
+			case "9":
+				cnt[9]++;
+				break;					
+			}
+		}
+		for(int i=0; i<cnt.length; i++) {
+			System.out.println(cnt[i]);
+		}
+		
+		
+		
+		
+	}
+
+}
